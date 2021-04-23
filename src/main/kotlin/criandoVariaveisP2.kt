92cd3fd19d71c225a4758966674acbaf8678986e
GuerraAna <ana.guerrads@hotmail.com>
2021-04-23 00:44:12 -0300

    Second Commit: criandoVariaveisP2

---
+++ src/main/kotlin/criandoVariaveisP2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Ana"
+    val numeroConta: Int = 1000
+    val saldo: Double = 0.0
+    //saldo = 2.5
+    //saldo = 100 + 2.0
+
+    println(titular)
+    println("$titular")
+    println("Número da Conta: $numeroConta")
+    println("Número do saldo: $saldo")
+}
