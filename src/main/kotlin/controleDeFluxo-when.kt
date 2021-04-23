b613a54933eb5035d6ec555536a1c8143e186f65
GuerraAna <ana.guerrads@hotmail.com>
2021-04-23 01:42:32 -0300

    Third Commit: ControleDeFluxo

---
+++ src/main/kotlin/controleDeFluxo-when.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Ana"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
+
+    //Segundo exemplo (Controle de fluxo when):
+    when {
+        saldo > 0.0 -> {
+            println("A conta é positiva.")
+        }
+        saldo == 0.0 -> {
+            println("A conta é neutra.")
+        }
+        else -> {
+            println("A conta é negativa.")
+        }
+    }
+
+}
--- src/main/kotlin/criandoVariaveis.kt
+++ src/main/kotlin/criandoVariaveis.kt
@@ -1,8 +1,12 @@
 fun main() {
+    //Palavra reservada constante não pode ser alterada
+    //Ex.: val entrada = "olá" [dá erro]
     val entrada = "Bem vindo ao Bytebank"
 
+    //Palavra reservada variável pode ser alterada;
+    //Ex.: var titurlar = "Clara" [não dá erro]
     var titular = "Ana"
-
+    
     //Primeiro Exemplo:
     println(titular)
 
