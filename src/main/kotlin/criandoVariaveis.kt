fb8ade697fc8f080a88453b3809da24ed3a34a5a
GuerraAna <ana.guerrads@hotmail.com>
2021-04-23 00:15:54 -0300

    First Commit: criandoVariaveis

---
+++ src/main/kotlin/criandoVariaveis.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val entrada = "Bem vindo ao Bytebank"
+
+    var titular = "Ana"
+
+    //Primeiro Exemplo:
+    println(titular)
+
+    //Segundo Exemplo:
+    println("$titular")
+
+    //Terceiro Exemplo:
+    println("$entrada $titular")
+
+    //Quarto Exemplo:
+    println("$entrada" + ": " + titular)
+}
