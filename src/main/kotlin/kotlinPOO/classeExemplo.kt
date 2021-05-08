22a1f2f174625924fa83bbe46830f1cbae529241
GuerraAna <ana.guerrads@hotmail.com>
2021-05-08 12:38:40 -0300

    Third POO Commit: Finish POO Classes

---
+++ src/main/kotlin/kotlinPOO/classeExemplo.kt
@@ -0,0 +1,62 @@
+package kotlinPOO
+
+fun main() {
+    val primeiraPessoa = Pessoa()
+    primeiraPessoa.titular = "Maria Clara"
+        println(primeiraPessoa.titular)
+    primeiraPessoa.numero = 1912
+        println(primeiraPessoa.numero)
+    primeiraPessoa.saldo = 245.50
+        println(primeiraPessoa.saldo)
+        println()
+        println("Primeira pessoa métodos em sua própria conta:")
+    primeiraPessoa.depositar(500.0)
+        println(primeiraPessoa.saldo)
+    primeiraPessoa.sacar(100.0)
+        println(primeiraPessoa.saldo)
+        println()
+
+    val segundaPessoa = Pessoa()
+    segundaPessoa.titular = "Pedro Henrique"
+        println(segundaPessoa.titular)
+    segundaPessoa.numero = 1994
+        println(segundaPessoa.numero)
+    segundaPessoa.saldo = 431.90
+        println(segundaPessoa.saldo)
+        println()
+        println("Segunda Pessoa métodos em sua própria conta:")
+    segundaPessoa.depositar(50.0)
+        println(segundaPessoa.saldo)
+    segundaPessoa.sacar(10.0)
+        println(segundaPessoa.saldo)
+        println()
+        println("Depósito da segunda pessoa para a primeira pessoa:")
+    segundaPessoa.transferencia(100.0, primeiraPessoa)
+        println(segundaPessoa.saldo)
+}
+
+class Pessoa() {
+    var titular: String = ""
+    var numero: Int = 0
+    var saldo:Double = 0.0
+
+    fun depositar(valor: Double) {
+            saldo += valor
+    }
+    fun sacar(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+    fun transferencia(valor: Double, destino: Pessoa): Boolean {
+        if (saldo >= valor) {
+            //Conta origin:
+            saldo -= valor
+            //Conta Destino:
+            destino.saldo += valor
+            return true
+        } else {
+            return false
+        }
+    }
+}
--- src/main/kotlin/kotlinPOO/contaAnaPoo.kt
+++ src/main/kotlin/kotlinPOO/contaAnaPoo.kt
@@ -2,29 +2,58 @@ package kotlinPOO
 
 // Códigos que seguem POO
 fun main() {
-    val nome = ContaBancaria()
-    nome.titular = "Ana Guerra POO"
-        println(nome.titular)
-    nome.conta =  2123
-        println(nome.conta)
-    nome.saldo = 2000.0
+    println("Byte Bank Casa")
+    val nomeFelipe = ContaBancaria()
+    val nomeAna = ContaBancaria()
+    nomeAna.titular = "Ana Guerra POO"
+        println(nomeAna.titular)
+    nomeAna.conta =  2123
+        println(nomeAna.conta)
+    nomeAna.saldo = 2000.0
 
         // Saldo atual:
             println("Saldo da conta de Ana.")
-            println(nome.saldo)
+            println(nomeAna.saldo)
 
         // Depósito:
             println("Depósito na conta da Ana.")
-        nome.depositar(20.0)
-            println(nome.saldo)
+        nomeAna.depositar(20.0)
+            println(nomeAna.saldo)
 
         // Saque:
             println("Saque da conta da Ana.")
-        nome.sacar(10.0)
-            println(nome.saldo)
+        nomeAna.sacar(10.0)
+            println(nomeAna.saldo)
 
         // Transferir:
+            println("Transferir dinheiro de Ana para Felipe.")
+        nomeAna.transferir(400.0, nomeFelipe)
+            println()
 
+    nomeFelipe.titular = "Felipe Guerra POO"
+        println(nomeFelipe.titular)
+    nomeFelipe.conta =  2124
+        println(nomeFelipe.conta)
+    nomeFelipe.saldo = 1000.0
+
+    // Saldo atual:
+        println("Saldo da conta de Felipe.")
+        println(nomeFelipe.saldo)
+
+    // Depósito:
+        println("Depósito na conta da Felipe.")
+    nomeFelipe.depositar(30.0)
+        println(nomeFelipe.saldo)
+
+    // Saque:
+        println("Saque da conta da Felipe.")
+    nomeFelipe.sacar(10.0)
+        println(nomeFelipe.saldo)
+
+    // Transferir:
+        println("Transferência da conta de Felipe para Ana.")
+    nomeFelipe.transferir(200.0, nomeAna)
+        println(nomeFelipe.saldo)
 }
 
 class ContaBancaria {
@@ -40,7 +69,12 @@ class ContaBancaria {
     fun depositar(valor: Double) {
         this.saldo += valor
     }
-    fun transferir() {
-
+    fun transferir(valor: Double, destino: ContaBancaria): Boolean {
+        if(saldo >= valor) {
+            saldo -= valor
+            destino.saldo += valor
+            return true
+        }
+            return false
     }
 }
